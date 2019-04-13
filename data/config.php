8e4fe292bc56b6398418bc4823fac880f60ae1ad
Sergey Melnikov <39922197+MelnikovSergey@users.noreply.github.com>
2019-04-13 09:24:30 +0300

    Update config.php

--- data/config.php
+++ data/config.php
@@ -1,3 +1,6 @@
 <?php
-define("DIR_PATH", S_SERVER['DOCUMENT_ROOT']);
+$root_folder = 'new-fm';
+define("DIR_PATH", S_SERVER['DOCUMENT_ROOT']."/$root_folder");
+define("CLASSES", 'data/classes');
+define("THEME", 'data/theme');
 ?>
