d26ecde570090bb7f489c37b8f23558d5438c9e6
Sergey Melnikov <39922197+MelnikovSergey@users.noreply.github.com>
2019-04-13 12:44:31 +0300

    Update config.php

--- data/config.php
+++ data/config.php
@@ -1,6 +1,6 @@
 <?php
 $root_folder = 'new-fm'; # not necessary for production
 define("DIR_PATH", $_SERVER['DOCUMENT_ROOT']."/$root_folder");
-define("CLASSES", 'data/classes');
-define("THEME", 'data/theme');
+define("CLASSES", 'data/classes/');
+define("THEME", 'data/theme/');
 ?>
