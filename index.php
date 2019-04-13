52de2ac81b5e4f8789b7cb821219197f1c78fb22
Sergey Melnikov <39922197+MelnikovSergey@users.noreply.github.com>
2019-04-13 10:15:23 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
 <?php
+header("Content-Type: text/html; charset='utf-8'");
+
+require_once("data/config.php");
+
+if ($_GET['do']) {
+	$name = strip_tags($_GET['do']);
+
+	if (file_exists(CLASSES.$name.'.php')) {
+		$class_name = Sname;
+	} else {
+		$class_name = "main";
+	}
+} else {
+	$class_name = "main";
+}
+
+echo $class_name;
 
 ?>
