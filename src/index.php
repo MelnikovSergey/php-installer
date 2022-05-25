455702d1cbc05695aebb99fe07a5d9f30e79a9a0
Sergey Melnikov <39922197+MelnikovSergey@users.noreply.github.com>
2022-05-25 23:47:55 +0300

    markup testing

--- src/index.php
+++ src/index.php
@@ -1,1 +1,48 @@
 <?php
+//..
+?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+		<title>Validation form</title>
+    <link rel='stylesheet' type='text/css' href='style.css'/>
+ 	</head>
+	<body>
+    <form name="installForm" class='installForm'>
+    <ul>
+        <li>
+           <label>Database:*</label> 
+           <input type="text" name="database" placeholder='Database name'> <br/>
+        </li>
+        <li>
+            <label>Username:*</label>
+            <input type='text' name='username'placeholder='Enter your name'> <br/>
+        </li>
+        <li>
+            <label>Password:* </label>
+            <input type='password' name='pwd1' placeholder='Enter your password'> <br/>
+        </li>
+        <li>
+            <label>Confirm password:*</label>
+            <input type='password' name='pwd2' placeholder='Re-type your password'> <br/>
+        </li>
+        <li>
+            <label>Email:*</label>
+            <input type='text' name='email' placeholder='Enter a valid email'> <br/>
+        </li>
+        <li>
+           <label>Database Host:*</label> 
+           <input type="text" name="database" placeholder='Database host'> <br/>
+        </li>
+        <li>
+           <label>Table Prefix:*</label> 
+           <input type="text" name="database" placeholder='Table prefix'> <br/>
+        </li>            
+        <li>
+           <button class="submit" type="submit">Submit</button>
+        </li>
+    </ul>
+    </form>
+  </body>
+</html>   
