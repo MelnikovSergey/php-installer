da435dc717a264f9e30be0a92a71dbde5753614f
Sergey Melnikov <39922197+MelnikovSergey@users.noreply.github.com>
2022-06-05 00:29:58 +0300

    checking the PHP version

--- index.php
+++ index.php
@@ -3,26 +3,9 @@ header("Content-Type: text/html; charset='utf-8'");
 
 require_once("data/config.php");
 
-if ($_GET['do']) {
-	$name = strip_tags($_GET['do']);
+$php_version = phpversion();
 
-	if (file_exists(CLASSES.$name.'.php')) {
-		$class_name = Sname;
-	} else {
-		$class_name = "main";
-	}
-} else {
-	$class_name = "main";
+if($php_version < 7) {
+  $error = true;
+  $php_error = "Your PHP version is $php_version. To continue working, you need to update your PHP version";
 }
-
-function __autoload($class_name) {
-
-	require_once(CLASSES.$class_name.'.php');
-}
-
-$obj = new $class_name;
-$res_arr = $obj->get_body();
-
-require THEME.'index.php');
-
-?>
