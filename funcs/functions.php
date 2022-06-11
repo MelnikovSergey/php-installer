ef6808f8490209ea00fffea627910f07e6031ffa
unknown <sergeyandmelnikov@gmail.com>
2022-06-11 23:07:50 +0300

    added db version check

---
+++ funcs/functions.php
@@ -0,0 +1,7 @@
+<?php
+
+function MySQL_version() {
+  $output = shell_exec('mysql -V');
+  preg_match('@[0-9]+\.[0-9]+\.[0-9]+@', $output, $version);
+  return @$version[0] ? $version[0] : -1;
+}
--- index.php
+++ index.php
@@ -2,10 +2,21 @@
 header("Content-Type: text/html; charset='utf-8'");
 
 require_once("data/config.php");
+require_once("funcs/functions.php");
 
 $php_version = phpversion();
 
 if($php_version < 7) {
   $error = true;
   $php_error = "Your PHP version is $php_version. To continue working, you need to update your PHP version";
+}
+
+$mysql_version = MySQL_version();
+
+if($mysql_version < 5.6) {
+	if($mysql_version == -1) {
+		$mysql_check = "MySQL version will be checked at the next step.";
+	} else {
+		$mysql_check = "Your MySQL version is $mysql_version. The application requires version 5.6 or later";
+	}
 }
